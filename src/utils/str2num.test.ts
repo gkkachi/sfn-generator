4f65c427275311d42bec389082fcf50fd52d8e08
Konosuke Kachi <github@s.k-kachi.net>
2022-02-02 23:23:41 +0900

    Fix test

---
+++ src/utils/str2num.test.ts
@@ -0,0 +1,24 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+import { Str2Num } from './str2num';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Cases = [
+  Expect<Equal<
+  Str2Num<'1'>,
+  1
+  >>,
+  Expect<Equal<
+  Str2Num<'23'>,
+  23
+  >>,
+  Expect<Equal<
+  Str2Num<'a'>,
+  never
+  >>,
+];
+
+describe('Str2Num', () => {
+  test('dummy', () => {
+    expect(true).toBe(true);
+  });
+});
